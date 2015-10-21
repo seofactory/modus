80e4d69d8b3511d599f2bfeb37d286937b0b572c
Hardrei <aiharitonov71@gmail.com>
2015-10-21 17:10:24 +0300

    Добавил анкету мастера как её видят клиенты

--- Личный кабинет/themes/sapota/js/site.js
+++ Личный кабинет/themes/sapota/js/site.js
@@ -74,15 +74,73 @@ function sizeBodyClient(){
 	});
 }
 
+//---------------------------------------------------------добавил после 14.10.15--------------------------------
+//центровка формы входа
+function sizeBodyInto(){
+	//определяем размеры окна браузера
+	var windowWidth = $(window).width();
+	var windowHeight = $(window).height();
+	
+	//определяем размеры картинки (бэкграунд)
+	var imageWidth = $('.intoSiteFormWrapper').width();
+	var imageHeight = $('.intoSiteFormWrapper').height();
+	
+	//вычисляем расстояние от краев окна браузера до центрального положения картинки
+	var positionX = windowWidth/2 - imageWidth/2;
+	var positionY = windowHeight/2 - imageHeight/2;
+	
+	//присваиваем координаты картинке
+	$('.intoSiteFormWrapper').offset({	
+		top: positionY,
+		left: positionX
+	});
+}
+
+//центровка большой картинки портфолио с описанием
+function portfolioBigImg(){
+	//определяем размеры окна браузера
+	var windowWidth = $(window).width();
+	var windowHeight = $(window).height();
+	var windowScroll = $(window).scrollTop();
+	
+	//определяем размеры картинки (бэкграунд)
+	var imageWidth = $('.portfolioPhotoPopUp').width();
+	var imageHeight = $('.portfolioPhotoPopUp').height();
+	
+	//вычисляем расстояние от краев окна браузера до центрального положения картинки
+	var positionX = windowWidth/2 - imageWidth/2;
+	var positionY = (windowHeight/2 - imageHeight/2) + windowScroll;
+	
+	//присваиваем координаты картинке
+	$('.portfolioPhotoPopUp').offset({	
+		top: positionY,
+		left: positionX
+	});
+}
+//---------------------------------------------------------END добавил после 14.10.15--------------------------------
+
 //центровка попапа при изменении размера окна браузера
 $(window).resize(function(){
 	sizeBody();
 	sizeBodyClient();
+	sizeBodyInto();//---------------------------------------------------------добавил после 14.10.15--------------------------------
+	portfolioBigImg();
 });
 
 window.onload = function(){
 	sizeBody();
 	sizeBodyClient();
+	sizeBodyInto();//---------------------------------------------------------добавил после 14.10.15--------------------------------
+	portfolioBigImg();
+	
+	$('.opinionText').children().not( $(".opinionTextWrapper")[0] ).hide();//скрыть все отзывы кроме первого при загрузке страницы
+	
+	//сокращаем текст описания О себе
+	var textLength = $('.oneself').text();
+	if(textLength.length > 250){
+		var shortText = textLength.slice(0, 250);
+		$('.oneself').hide().after('<p class="onselfShort">' + shortText + '...</p>');
+	}
 }
 
 //активируем попап "Информация сохранена"
@@ -154,6 +212,14 @@ $(document).ready(function(){
 		}
 	})
 	
+//---------------------------------------------------------добавил после 14.10.15--------------------------------	
+	//активация формы входа
+	$('.getInto').on('click', function(){
+		$('.intoSiteFormWrapperAll').show();
+		sizeBodyInto();
+	});
+//---------------------------------------------------------END добавил после 14.10.15--------------------------------
+	
 	//переключение вкладок мастер, салон красоты
 	$('.changeMasterSalon span').on('click', function(){
 		$('.changeMasterSalon span.value').removeClass('red');
@@ -191,6 +257,16 @@ $(document).ready(function(){
 		$('.registrationClientFormWrapper').hide();
 		$('.registrationClientForm').removeAttr('style');
 	});
+	
+	$('.portfolioPhotoPopUpWrapper .close_button').on('click', function(){
+		$('.portfolioPhotoPopUpWrapper').hide();
+		$('.portfolioPhotoPopUp').removeAttr('style');
+	});
+//---------------------------------------------------------добавил после 14.10.15--------------------------------	
+	$('.intoSiteFormWrapperAll .close_button').on('click', function(){
+		$('.intoSiteFormWrapperAll').hide();
+	});
+//---------------------------------------------------------END добавил после 14.10.15--------------------------------	
 	
 	//переключение вкладок Что мы предлагаем?, Как это работает?
 	$('.whatWeOffered').on('click', function(){
@@ -242,10 +318,10 @@ $(document).ready(function(){
 	$('.selectBox').on('click', function(){
 		$(this).siblings('.listServicesShow').find('.addList').hide();
 		if($(this).next('.listServices').hasClass('listServicesShow')){
-			$(this).next('.listServices').hide().removeClass('listServicesShow');;
+			$(this).next('.listServices').slideUp().removeClass('listServicesShow');;
 			$(this).children().children().removeClass('openList');
 		} else {
-			$(this).next('.listServices').show().addClass('listServicesShow');
+			$(this).next('.listServices').slideDown().addClass('listServicesShow');
 			$(this).children().children().addClass('openList');
 		}	
 	});
@@ -316,15 +392,152 @@ $(document).ready(function(){
 	$('.services > span').on('click', function(){
 		if($(this).siblings('.selectBox').hasClass('.selectBox')){
 			$(this).removeClass('active');
-			$(this).children().removeClass('openList');
-			$(this).siblings('.selectBox').hide().removeClass('.selectBox');
+			$(this).children().removeClass('openList fa-caret-down').addClass('fa-caret-right');
+			$(this).siblings('.selectBox').slideUp().removeClass('.selectBox');
 			$(this).siblings('.listServices').hide();
 		}else{
 			$(this).addClass('active');
-			$(this).children().addClass('openList');
-			$(this).siblings('.selectBox').show().addClass('.selectBox');
+			$(this).children().addClass('openList fa-caret-down').removeClass('fa-caret-right');;
+			$(this).siblings('.selectBox').slideDown().addClass('.selectBox');
 		}
 		
 	})
 /*----------END Прайс-лист----------*/	
+
+/*----------скрипты для портфолио----------*/
+	//закрашиваем звездочки рейтинга
+	//только для теста с промпт, потом раскомментить ниже
+	var promptRating = prompt('Введите значение рейтинга:','');
+	var ratingText = $('.ratingNumber').text(promptRating);
+	var y = promptRating.replace(",", ".");
+	var ratingNumber = parseFloat(y);
+	var imgLength = $('.ratingMasterBox .ratingStarsColor img').width();
+	var ratingLength = (ratingNumber * imgLength)/5;    
+	$('.ratingMasterBox .ratingStarsColor').width(ratingLength);
+	//END только для теста с промпт
+	
+	/*var ratingText = $('.ratingNumber').text();       //получаем текстовое значение рейтинга
+	var y = ratingText.replace(",", ".");               //преобразуем запятую в точку
+	var ratingNumber = parseFloat(y);                   //преобразуем текст в число
+	var imgLength = $('.ratingStarsColor img').width(); //получаем длину элемента для формулы
+	var ratingLength = (ratingNumber * imgLength)/5;    //вычисляем длину закрашенного фона
+	$('.ratingStarsColor').width(ratingLength);*/
+	
+	//закрашиваем звездочки оценки по отзыву (для всех отзывов)
+	$('.opinionTextWrapper').each(function(){
+		var jobEvaluationText = $(this).find('.jobEvaluation').text(); //получаем текстовое значение оценки
+		var y = jobEvaluationText.replace(",", ".");                   //преобразуем запятую в точку 
+		var evaluationNumber = parseFloat(y);                          //преобразуем текст в число
+		var imgLength = $(this).find('.ratingStarsColor img').width(); //получаем длину элемента для формулы
+		var evaluationLength = (evaluationNumber * imgLength)/5;       //вычисляем длину закрашенного фона
+		$(this).find('.ratingStarsColor').width(evaluationLength);
+	});                                                 
+	
+	//показать полное описание О себе
+	$('.showAllOnself').on('click',function(e){
+		e.preventDefault();
+		if($(this).text() == 'показать полностью'){
+			$('.onselfShort').hide();
+			$('.oneself').show();
+			$(this).text('Скрыть текст');
+		} else {
+			$('.onselfShort').show();
+			$('.oneself').hide();
+			$(this).text('показать полностью');
+		}
+		
+	});
+	
+	//показ стоимости дополнительных услуг
+	$('.costAddServiceLink').on('click',function(e){
+		e.preventDefault();
+		if($('.costAddServiceBox').hasClass('showCostAddServiceBox')){
+			$('.costAddServiceBox').slideUp('slow').removeClass('showCostAddServiceBox');
+			$(this).text('стоимость дополнительных услуг');
+		} else {
+			$('.costAddServiceBox').slideDown('slow').addClass('showCostAddServiceBox');
+			$(this).text('свернуть дополнительные услуги');
+		}
+		
+	});
+	
+	//показ всех фотографий в портфолио мастера
+	$('.showAllPhotosPortfolio').on('click',function(e){
+		e.preventDefault();
+		if($('.photosPortfolio').hasClass('allPhotosShow')){
+			$('.photosPortfolio').removeClass('allPhotosShow');	
+		} else {
+			$('.photosPortfolio').addClass('allPhotosShow');
+		}	
+	});
+	
+	//показ формы Хочу также при клике на фотографию из порфолио
+	$('.photosPortfolio img').each(function(){
+		$(this).on('click',function(){
+			$('.wantAlso button').removeClass('buttonClick');
+			$('.formWantAlsoWrapper').hide();
+			$('.portfolioPhotoPopUpWrapper').show();
+			portfolioBigImg();
+		});	
+	});
+	
+	//Показать все работы
+	var showPhotos = true;
+	$('.showAllPhotosPortfolio').on('click', function(e){
+		e.preventDefault();
+		if(showPhotos == true){
+			$('.photosPortfolio').find('.portfolioImgHide').slideDown('slow');
+			$(this).text('Свернуть портфолио');
+			showPhotos = false;
+		} else if(showPhotos == false){
+			$('.photosPortfolio').find('.portfolioImgHide').slideUp('slow');
+			$(this).text('Показать все работы');
+			showPhotos = true;		
+		}
+		
+	});
+	
+	//Показать все отзывы
+	var count = true;
+	$('.showAllOpinion').on('click', function(e){
+		e.preventDefault();
+		if(count == true){
+			$('.opinionText').children().slideDown('slow');
+			$(this).text('Свернуть отзывы');
+			count = false;
+		} else if(count == false){
+			$('.opinionText').children().not( $(".opinionTextWrapper")[0] ).slideUp('slow');
+			$(this).text('Показать все отзывы');
+			count = true;		
+		}
+		
+	});
+	
+	//Показать все районы выезда
+	var showArea = false;
+	$('.showAllArea').on('click', function(e){
+		e.preventDefault();
+		if(showArea == false){
+			$(this).parent().find('.lastVisibleArea').nextAll().slideDown();
+			$(this).text('Свернуть все');	
+			showArea = true;
+		} else {
+			$(this).parent().find('.lastVisibleArea').nextAll().not($('.showAllArea')).slideUp();
+			$(this).text('Показать все');
+			showArea = false;
+		}		
+	});
+	
+	//Показать номер телефона
+	$('.showPhoneNumberMaster').on('click', function(){
+		$(this).hide();
+		$(this).parents('.phoneBox').find('.phoneNumberMaster').show();
+	});
+	
+	//Показать форму Хочу также
+	$('.wantAlso button').on('click', function(){
+		$(this).addClass('buttonClick');
+		$('.formWantAlsoWrapper').slideDown('slow');
+	});
+/*----------END скрипты для портфолио----------*/
 })
